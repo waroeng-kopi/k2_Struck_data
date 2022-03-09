f2c380ef8569e02d1ffb33f7a6a066265bd04ff3
Adamjatim <adamrahmatilahi@gmail.com>
2022-03-09 13:41:21 +0700

    Update 09/03/2022

--- week_01/soal_01.cpp
+++ week_01/soal_01.cpp
@@ -11,6 +11,8 @@
 */
 
 #include <iostream>
+#include <string>
+
 
 struct Data_Pegawai {
     std::string nama, 
@@ -26,12 +28,48 @@ struct Data_Pegawai {
     }tangal_masuk;
 };
 
+void Error_msg(){
+    std::cout << "Terjadi masalah pada inputan, periksa kembali kesalahan." << std::endl;
+}
+
 int main(void){
-    int input;
+    int input, i;
 
     std::cout << "PROGRAM DATA PEGAWAI\n\n";
     std::cout << "Inputkan jumlah pegawai : ";
     std::cin >> input;
 
-    struct 
+    Data_Pegawai pegawai[input];
+
+    for(i = 0; i < input; i++) {
+        std::cout << "Input nama : "; fflush(stdin);
+        std::getline(std::cin, pegawai[i].nama);
+        std::cout << "Input npp : ";
+        std::getline(std::cin, pegawai[i].npp);
+        std::cout << "Input alamat : ";
+        std::getline(std::cin, pegawai[i].alamat);
+        std::cout << "Input jenis kelamin (p = pria / w = wanita) : ";
+        std::getline(std::cin, pegawai[i].jenis_kelamin);
+        for (int j = 0; j < pegawai[i].jenis_kelamin.length(); j++){
+            pegawai[i].jenis_kelamin[j] = tolower(pegawai[i].jenis_kelamin[j]);
+            if (strcmp(pegawai[i].jenis_kelamin[j],"p")==0 || strcmp(pegawai[i].jenis_kelamin[j],"w")==0){
+                break;
+            }
+            else{
+                std::cout << "input haruslah 'p' atau 'w'" << std::endl;
+            }
+        }
+        
+        while(true){
+            std::cout << "Input status pegawai (tetap / honorer) : ";
+            std::getline(std::cin, pegawai[i].status_pegawai);
+            if(strcmp(pegawai[i].status_pegawai,"tetap")==0 || strcmp(pegawai[i].status_pegawai,"honorer")==0){
+                break;
+            }
+            else{
+                std::cout << "input haruslah 'tetap' / 'honorer'" << std::endl;
+            }           
+        }
+
+    }
 }
